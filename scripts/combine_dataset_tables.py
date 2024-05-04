dbd522dcb36d7b84cb97b57e8992fbe9e023d712
Markus Haak <markus.haak@posteo.de>
2024-05-04 12:24:24 +0200

    update datasets

--- scripts/combine_dataset_tables.py
+++ scripts/combine_dataset_tables.py
@@ -6,7 +6,7 @@ import pandas as pd
 scipt_path = os.path.dirname(__file__)
 
 required_cols = [
-    'dataset', 'sample', 'NAT_source', 'ref_source', 'WGA_source', 'priority'
+    'dataset', 'sample', 'NAT_source', 'ref_source'#, 'WGA_source', 'priority'
     ]
 
 # parse individual dataframes
@@ -20,10 +20,14 @@ df = pd.concat(dfs)
 
 # parse dataset information from manual curation or other sources
 for fp in glob.glob(os.path.join(scipt_path, "../manual_curation/*.csv")):
-    d = pd.read_csv(fp, index_col=0)
+    d = pd.read_csv(fp)
     d['ID'] = d['dataset'] + "/" + d['sample']
     d = d.set_index('ID', drop=True)
     for col in d.columns:
-        df.loc[d.index, col] = d[col]
+        if col not in ['ID', 'dataset', 'sample']:
+            df.loc[d.index, col] = d[col]
 
-df.to_csv(os.path.join(scipt_path, "../all_datasets.csv"))
+sel = ~pd.isna(df[required_cols[0]])
+for col in required_cols:
+    sel &= ~pd.isna(df[col])
+df.loc[sel].to_csv(os.path.join(scipt_path, "../all_datasets.csv"))
